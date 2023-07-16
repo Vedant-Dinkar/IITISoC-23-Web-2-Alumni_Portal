19a67690e75f659fd899ffb533aebae6a48e21f5
riddhi2765 <riddhichandekar2108@gmail.com>
2023-07-16 16:40:31 +0530

    gallery images can be uploaded

--- Admin/main2.py
+++ Admin/main2.py
@@ -7,6 +7,7 @@ from email.message import EmailMessage
 import ssl
 import smtplib
 from datetime import date
+from pymongo import errors
 
 
 
@@ -73,23 +74,35 @@ def gallerysext():
         image_file = request.files['gallery_image']
         file_data=image_file.read()
         fs.put(file_data,filename=event_gallery_name)
-        return redirect(url_for('gallery'))
+        return redirect(url_for('gallerydisplay'))
     return render_template('galleryext.html')
 
-# @app.route('/gallery')
-# def gallery():
-#     all_pics = mongo.db.Alumni_Admin.find()
-#     return render_template('gallery.html', pics=all_pics)
-
 @app.route('/gallery')
-def gallery():
-    images=fs.find()
-    return render_template('gallery.html', images=images)
+def gallerydisplay():
+    try:
+        # Retrieve all image files from GridFS
+        files = fs.find()
+        image_names = []
+        for file in files:
+            image_names.append(file.filename)
+        return render_template('gallery.html', image_names=image_names)
+    except errors.ServerSelectionTimeoutError:
+        return 'Failed to connect to MongoDB server'
+    
+@app.route('/display/<filename>')
+def display_image(filename):
+    try:
+        # Retrieve the image from GridFS by filename
+        file = fs.find_one({'filename': filename})
+        if file is None:
+            return 'Image not found'
+        else:
+            # Set the content type and return the image data
+            response = app.response_class(file.read(), mimetype='image/jpeg')
+            return response
+    except errors.ServerSelectionTimeoutError:
+        return 'Failed to connect to MongoDB server'
 
-
-@app.route('/auth')
-def auth():
-    return render_template('layout.html')
 
 
 
